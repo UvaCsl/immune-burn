799a3b91ae71589948dc78686317a9d2fb756f1d
Alva <avpresbitero@gmail.com>
2021-12-01 15:40:19 +0800

    Add MARS data. Add EDA

--- burn/plot/plot.py
+++ burn/plot/plot.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import burn
+import numpy as np
 
 from burn.utils import *
 
@@ -12,12 +13,12 @@ def get_header():
               'M_R', 'M_A', 'CH', 'N_A', 'ND_A', 'ACH', 'ND_N', 'C_blood', 'C', 'C_A', 'C_B', 'TGF_beta']
     return header
 
-def prep_plot(wsol, t_interval, timein, timeout):
+def prep_plot(wsol, t_interval, timein, timeout, time_format=1):
     t = set_time(t_interval, timein, timeout)
     df_model = pd.DataFrame(wsol)
     keys = df_model.columns.values
     dictionary = dict(zip(keys, get_header()))
     df_model = df_model.rename(columns=dictionary)
-    df_model['Time'] = t
+    df_model['Time'] = np.array(t) * time_format
     
     return df_model, t
--- burn/preprocess/cytokines/process_cytokines.py
+++ burn/preprocess/cytokines/process_cytokines.py
@@ -1,10 +1,11 @@
 def get_med_mad(cytokine, df, params, to_calculate = ['median', 'mad']):
     mw = params[cytokine]
+    time_format = params['time_format']
     mult = ((1*10**-12)*(6.02*10**23)/((1*10**-3)*mw*10**6))
     df[cytokine] = df[cytokine] * mult
     df_cyto = df[[cytokine, 'Day']].groupby('Day').agg({cytokine:to_calculate})
     df_cyto.columns = df_cyto.columns.droplevel(0)
     df_cyto = df_cyto.reset_index()
-    df_cyto['time'] = df_cyto.Day * 24
+    df_cyto['time'] = df_cyto.Day * time_format
     df_cyto = df_cyto.drop('Day', axis=1)
     return df_cyto
